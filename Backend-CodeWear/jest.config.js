596b2c988d1fe75c8ecf47fc7347034137ab2658
JeffTads2025 <jefftads2025@gmail.com>
2026-06-07 18:08:48 -0300

    chore(testes): estabiliza pre-push com timeout maior e execucao serial

--- Backend-CodeWear/jest.config.js
+++ Backend-CodeWear/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   rootDir: './src',
+  testTimeout: 30000,
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
